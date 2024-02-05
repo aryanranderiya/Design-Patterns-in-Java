d6108cb4fdb5547bc3dc5d0bfdd20db55a845699
Aryan <64796509+aryanranderiya@users.noreply.github.com>
2024-02-05 11:45:03 +0530

    Prototype Design Pattern

--- Pr3 Prototype/Pr3_Prototype.java
+++ Pr3 Prototype/Pr3_Prototype.java
@@ -18,27 +18,31 @@ class Car {
         this.color = color;
     }
 
+    // Getter Method
     public int getID() {
         return id;
     }
 
+    // Getter Method
     public String getBrand() {
         return brand;
     }
 
+    // Getter Method
     public String getModel() {
         return model;
     }
 
+    // Getter Method
     public int getYear() {
         return year;
     }
 
+    // Getter Method
     public String getColor() {
         return color;
     }
 
-    // The toString() method returns the String representation of the object.
     // The toString() method returns the String representation of the object.
     // In this implementation, the Java compiler internally invokes the toString()
     // method and prints the attributes of the car accordingly.
@@ -57,24 +61,31 @@ class Car {
 
 class CarShop implements Cloneable {
     private String shopName;
+
+    // List to store the cars in the car shop
     List<Car> carList = new ArrayList<>();
 
+    // Setter method to set the shop name
     public void setShopName(String shopName) {
         this.shopName = shopName;
     }
 
+    // Getter method to fetch the shop name
     public String getShopName() {
         return shopName;
     }
 
-    public List<Car> getCarsList() {
-        return carList;
-    }
-
+    // Setter method to set the list of cars
     public void setCarsList(List<Car> carList) {
         this.carList = carList;
     }
 
+    // Getter method to fetch the list of cars
+    public List<Car> getCarsList() {
+        return carList;
+    }
+
+    // Overridden method to providestring representation of the CarShop object
     public String toString() {
         return "CarShop{ Shop Name = " + shopName + ", " + "Cars = " + carList + " }";
     }
@@ -84,27 +95,15 @@ class CarShop implements Cloneable {
         getCarsList().add(c);
     }
 
-    // Shallow Clone
+    // Deep Clone
     protected Object clone() throws CloneNotSupportedException {
         return super.clone();
     }
 
-    // protected CarShop clone() throws CloneNotSupportedException {
-    // // CarShop cloned = (CarShop) super.clone();
-    // // cloned.carList = new ArrayList<>(this.carList);
-    // // return cloned;
-    // CarShop obj = new CarShop();
-    // for (Car car : carList) {
-
-    // obj.getCarsList().add(car);
-    // }
-    // return obj;
-    // }
-
-    // Deep Clone
-    public CarShop deepClone(CarShop shop) {
-        CarShop obj = new CarShop();
-        obj.carList = shop.carList;
+    // Shallow Clone
+    public CarShop shallowClone(CarShop shop) {
+        CarShop obj = new CarShop(); // Create a new car shop object
+        obj.carList = shop.carList; // Assign the reference of the old carshop to the new carshop
         return obj;
     }
 
@@ -118,22 +117,24 @@ public class Pr3_Prototype {
         car_shop1.createCar(1, "brandA", "modelA", 2023, "Black");
         car_shop1.createCar(2, "brandB", "modelB", 2023, "Black");
 
-        // Shallow cloning. Modified Results will not be reflected in book shop 2
-        System.out.println("---------------------- Shallow Clone ---------------------- ");
+        // Deep cloning. Modified Results will not be reflected in book shop 2
+        System.out.println("--------------------- Deep Clone (Simply copies elements) ------------------ ");
         CarShop car_shop2 = (CarShop) car_shop1.clone();
         car_shop2.setShopName("Car Shop 2");
         System.out.println(car_shop1);
         System.out.println(car_shop2);
 
-        // Deep Clone. Modified Results will all be reflected.
-        System.out.println("---------------------- Deep Clone ---------------------- ");
+        // Shallow Clone. Modified Results will all be reflected.
+        System.out.println("------------------ Shallow Clone (Changes are reflected) ------------------ ");
         System.out.println("---------------------- Before Removing ---------------------- ");
         System.out.println(car_shop1);
 
-        CarShop car_shop3 = car_shop1.deepClone(car_shop1);
+        // Shallow clone shop 1 and assign it to shop 3
+        CarShop car_shop3 = car_shop1.shallowClone(car_shop1);
+        // Remove the first element of shop1 to see if changes are reflected in shop3
         car_shop1.getCarsList().remove(0);
         car_shop3.setShopName("Car Shop 3");
-        
+
         System.out.println("---------------------- After Removing ---------------------- ");
         System.out.println(car_shop1);
         System.out.println(car_shop3);
